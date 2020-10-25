85f56f9608a3f62709448ef67a16ec928baa174f
jonas <jonas.petraska7@gmail.com>
2020-10-25 21:19:47 +0200

    fin

--- config.js
+++ config.js
@@ -7,7 +7,8 @@ const connectionString = `postgresql://${process.env.DB_USER}@${process.env.DB_H
 const connection = `postgres://xmyeapemjodjjj:116d7b804f711ccfd90b4a141c9269d51ef3737dbf4d043acf8975ec5e3da9e7@ec2-54-158-222-248.compute-1.amazonaws.com:5432/d5afuc7q88b9tb`
 
 const pool = new Pool({
-    connectionString: connection
+    connectionString: connection,
+    ssl: { rejectUnauthorized: false }
 })
 
 module.exports = {pool}
