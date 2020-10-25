0c19a122ba77018b8a53722a158239823b275ab2
jonas <jonas.petraska7@gmail.com>
2020-10-25 21:11:26 +0200

    88

--- config.js
+++ config.js
@@ -4,10 +4,10 @@ const {Pool} = require('pg')
 const isProduction = true
 
 const connectionString = `postgresql://${process.env.DB_USER}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+const connection = `postgres://xmyeapemjodjjj:116d7b804f711ccfd90b4a141c9269d51ef3737dbf4d043acf8975ec5e3da9e7@ec2-54-158-222-248.compute-1.amazonaws.com:5432/d5afuc7q88b9tb`
 
 const pool = new Pool({
-    connectionString: isProduction ? process.env.DB_DATABASE_URL : connectionString,
-    ssl: isProduction
+    connectionString: connection
 })
 
 module.exports = {pool}
